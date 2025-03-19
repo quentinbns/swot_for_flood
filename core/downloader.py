d877bf98300b34e9062e70d3ed15032b9903fb93
qbonassies <qbonassies@gmail.com>
2025-03-19 15:33:39 +0100

    [core] Adding workspace pathes into project class

--- core/downloader.py
+++ core/downloader.py
@@ -8,15 +8,25 @@ import concurrent.futures
 class Downloader():
     """ Utility class to download data from the Earth Engine API.
     """
-    def __init__(self, download_path: Path, first_time: str, last_time: str, AOI: gpd.GeoDataFrame, passes: list=None, nodes:int=4):
-        """_summary_
+    def __init__(
+        self, 
+        download_path: Path, 
+        first_time: str, 
+        last_time: str, 
+        AOI: gpd.GeoDataFrame, 
+        do_download: bool = False,
+        download_type:str="PIXC",
+        passes: list=None, 
+        nodes:int=4
+        )-> None:
+        """Initialize the Downloader class
 
         Args:
-            first_date (str): _description_
-            last_date (str): _description_
-            AOI (gpd.GeoDataFrame): _description_
-            passes (list, optional): _description_. Defaults to None.
-            nodes (int, optional): _description_. Defaults to 4.
+            first_date (str): string with the first date in the format 'YYYY-MM-DD'
+            last_date (str): string with the last date in the format 'YYYY-MM-DD'
+            AOI (gpd.GeoDataFrame): GeoDataFrame of the Area of interest
+            passes (list, optional): list of SWOT passes to download. Defaults to None.
+            nodes (int, optional): number of nodes to use for parallel download. Defaults to 4.
         """
         self.download_path = download_path
         self.first_time = first_time
@@ -33,7 +43,22 @@ class Downloader():
         self.results = None
         self.passes = passes
         
-    def search_data(self, short_name):
+        self.download_type = download_type
+        
+        if do_download:
+            self.automatic_download()
+        else:
+            print('No automatic download, please use the Downloader object to download the data')
+        
+    def search_data(self, short_name:str):
+        """main function to search data from the Earth Engine API
+        
+        Args:
+            short_name (str): Shhort name of the SWOT data product to search
+        
+        Raises:
+            Exception: login failed, please check your credentials of the earthaccess module
+        """
         try:
             earthaccess.login()
         except Exception as e:
@@ -56,25 +81,42 @@ class Downloader():
         self.results = results
     
     def search_PIXC(self):
-        """_summary_
+        """search for SWOT_L2_HR_PIXC_2.0 data
         """
         self.search_data('SWOT_L2_HR_PIXC_2.0')
     
     def search_LakeSP(self):
-        """_summary_
+        """search for SWOT_L2_HR_LakeSP_2.0 data
         """
         self.search_data('SWOT_L2_HR_LakeSP_2.0')
     
     def search_Nodes(self):
-        """_summary_
+        """search for SWOT_L2_HR_Nodes_2.0 data
         """
         self.search_data('SWOT_L2_HR_Nodes_2.0')
+    
+    def automatic_download(self):
+        """automatic download of the data from the search results
+        
+        Raises:
+            Exception: Unknown download type
+        """
+        match self.download_type:
+            case 'PIXC':
+                self.search_PIXC()
+            case 'LakeSP':
+                self.search_LakeSP()
+            case 'Nodes':
+                self.search_Nodes()
+            case _:
+                raise Exception('Unknown download type')
+        self.download_pool()
 
-    def download(self, item):
-        """_summary_
+    def download(self, item:earthaccess.results.DataGranule):
+        """download a single item from the Earth Engine API
 
         Args:
-            item (_type_): _description_
+            item (earthaccess.results.DataGranule): item from the search results
         """
         if 'PIXC' in item['meta']['native-id']:
             path_file = self.download_path.joinpath(item['meta']['native-id'] +".nc")
@@ -86,10 +128,10 @@ class Downloader():
             print(f"File {item['meta']['native-id']} already exists")
             
     def download_pool(self):
-        """_summary_
+        """download all the data from the search results in parallel
 
         Raises:
-            Exception: _description_
+            Exception: no data to download, please search data first
         """
         if self.results is None:
             raise Exception('No data to download, please search data first')
@@ -105,10 +147,10 @@ class Downloader():
         concurrent.futures.thread._threads_queues.clear()
         
     def download_granules(self):
-        """_summary_
+        """download all the data from the search results in series
 
         Raises:
-            Exception: _description_
+            Exception: no data to download, please search data first
         """
         if self.results is None:
             raise Exception('No data to download')
--- core/swot_project.py
+++ core/swot_project.py
@@ -9,15 +9,49 @@ class SWOT_PROJECT():
     """
     TODO: Add description
     """
-    def __init__(self, project: str, workspace: Path, data_path: Path, CRS: str, first_time: str, last_time: str, aoi: str, aoi_crs: str=None, passes:list=None, nodes:int=4):
+    def __init__(
+        self, 
+        project: str, 
+        workspace: Path, 
+        data_path: Path, 
+        CRS: str, 
+        first_time: str, 
+        last_time: str, 
+        aoi: str, 
+        download : bool = False,
+        download_type: str = 'PIXC',
+        variables: list = [
+            'sig0', 
+            'coherent_power', 
+            'incidence', 
+            'gamma_tot', 
+            'gamma_SNR', 
+            'gamma_est', 
+            'power_plus_y', 
+            'power_minus_y',  
+            'interf_real', 
+            'interf_imag', 
+            'height', 
+            'classification', 
+            'bright_land_flag'
+            ],
+        aoi_crs: str=None, 
+        passes:list=None, 
+        nodes:int=4
+        ) -> None:
         """_summary_
 
         Args:
-            project (str): _description_
-            workspace (Path): _description_
-            data_path (Path): _description_
-            CRS (str): _description_
-            aoi (str): _description_
+            project (str): string with the name of the project
+            workspace (Path): string with the path to the workspace
+            data_path (Path): string with the path to the data folder
+            CRS (str): string with the CRS of the project
+            first_time (str): string with the first date in the format 'YYYY-MM-DD'
+            last_time (str): string with the last date in the format 'YYYY-MM-DD'
+            aoi (str): string with the name of the AOI file path
+            aoi_crs (str, optional): string with the CRS of the AOI. Defaults to None.
+            passes (list, optional): list of SWOT passes to download. Defaults to None.
+            nodes (int, optional): number of nodes to use for parallel download. Defaults to 4.
         """
         self.project : str =  project
         self.workspace : Path = workspace
@@ -25,15 +59,61 @@ class SWOT_PROJECT():
         self.CRS : str = CRS
         self.first_time : str = first_time
         self.last_time : str = last_time
+        self.variables : list = variables
 
         self.define_paths()
+        self.check_paths()
         self.open_aoi(aoi, aoi_crs)
         
-        self.Downloader = Downloader(download_path=self.SWOT_PATH, first_time=self.first_time, last_time=self.last_time, AOI=self.AOI, passes=passes, nodes=nodes)
-        
-        
-    def define_paths(self):
+        self.Downloader = Downloader(
+            download_path=self.SWOT_PATH,
+            first_time=self.first_time,
+            last_time=self.last_time,
+            AOI=self.AOI,
+            do_download=download,
+            download_type=download_type,
+            passes=passes,
+            nodes=nodes,
+            )
+               
+    def check_paths(self):
         """_summary_
+        """
+        if self.workspace.exists() is False:
+            self.workspace.mkdir()
+        if self.data_path.exists() is False:
+            self.data_path.mkdir()
+        if self.AUX_PATH.exists() is False:
+            self.AUX_PATH.mkdir()
+        if self.SWOT_PATH.exists() is False:
+            self.SWOT_PATH.mkdir()
+        if self.S6_PATH.exists() is False:
+            self.S6_PATH.mkdir()
+        if self.S1_PATH.exists() is False:
+            self.S1_PATH.mkdir()
+        if self.S2_PATH.exists() is False:
+            self.S2_PATH.mkdir()
+        if self.S3_PATH.exists() is False:
+            self.S3_PATH.mkdir()
+        if self.JASON_PATH.exists() is False:
+            self.JASON_PATH.mkdir()
+        if self.FABDEM_PATH.exists() is False:
+            self.FABDEM_PATH.mkdir()
+        if self.INSITU_PATH.exists() is False:
+            self.INSITU_PATH.mkdir
+        if self.PATH_GPKG.exists() is False:
+            self.PATH_GPKG.mkdir()
+        if self.TIFF_PATH.exists() is False:
+            self.TIFF_PATH.mkdir()
+        for variable in self.variables:
+            variable_path = self.TIFF_PATH.joinpath(variable)
+            if variable_path.exists() is False:
+                variable_path.mkdir()
+        if self.PLOT_PATH.exists() is False:
+            self.PLOT_PATH.mkdir()
+
+    def define_paths(self):
+        """define the paths of the project
         """
         self.PROJECT_PATH : Path = self.workspace.joinpath(self.project)
         self.SWOT_PATH : Path = self.data_path.joinpath('SWOT', self.project)
@@ -45,6 +125,9 @@ class SWOT_PROJECT():
         self.JASON_PATH : Path = self.data_path.joinpath('JASON', self.project)
         self.FABDEM_PATH : Path = self.data_path.joinpath('FABDEM', self.project)
         self.INSITU_PATH : Path = self.data_path.joinpath('insitu', self.project)
+        self.PATH_GPKG : Path = self.PROJECT_PATH.joinpath('gpkg_combined')
+        self.TIFF_PATH : Path = self.PROJECT_PATH.joinpath('rasters')
+        self.PLOT_PATH : Path = self.PROJECT_PATH.joinpath('plots')
         
     def open_aoi(self, aoi: str, aoi_crs: str=None):
         """_summary_
